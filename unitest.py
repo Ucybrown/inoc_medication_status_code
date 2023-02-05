deb6402f64ec2d836bcb37bb35e1906a7feaa0c8
Ucybrown <94966327+Ucybrown@users.noreply.github.com>
2023-02-05 17:57:06 +0100

    Update unitest.py

--- unitest.py
+++ unitest.py
@@ -1,42 +1,34 @@
 import unittest
-import code
+import json
+import pandas as pd
 
-class TestDataImport(unittest.TestCase):
-    def test_import(self):
-        data = code.import_data()
-        self.assertIsNotNone(data)
-        self.assertIsInstance(data, pandas.DataFrame)
-        self.assertGreater(data.shape[0], 0)
+# Import the data from the JSON file
+with open('patient_data.json') as json_file:
+    patient_data = json.load(json_file)
 
-class TestDataManipulation(unittest.TestCase):
-    def setUp(self):
-        self.data = code.import_data()
+# Convert the imported data to a DataFrame
+patient_df = pd.DataFrame(patient_data)
 
-    def test_manipulation(self):
-        manipulated_data = code.manipulate_data(self.data)
-        self.assertIsNotNone(manipulated_data)
-        self.assertIsInstance(manipulated_data, pandas.DataFrame)
-        self.assertEqual(manipulated_data.shape, (self.data.shape[0], 2))
+class TestDashApp(unittest.TestCase):
+    def test_update_scatter_plot(self):
+        # Test that the scatter plot updates correctly based on the selected room number
+        selected_room_number = '102'
+        filtered_df = patient_df[patient_df['room_number'] == selected_room_number]
+        scatter_plot = update_scatter_plot(selected_room_number)
+        self.assertEqual(scatter_plot['data'][0]['x'], filtered_df['room_number'].tolist())
 
-class TestScatterPlot(unittest.TestCase):
-    def setUp(self):
-        self.data = code.import_data()
-        self.manipulated_data = code.manipulate_data(self.data)
+    def test_show_patient_info(self):
+        # Test that the patient information is displayed correctly when clicking on a patient's name
+        clickData = {'points': [{'customdata': [1, 'John', 'Doe', '123 Main St', 'Male', 30, 'Medication 1', 'Medication 2', 'Medication 3', '2022-01-01', '2022-01-10']}]}
+        patient_info = show_patient_info(clickData)
+        self.assertEqual(patient_info[1]['children'], 'Patient ID: 1')
+        self.assertEqual(patient_info[2]['children'], 'Name: John Doe')
+        self.assertEqual(patient_info[3]['children'], 'Address: 123 Main St')
+        self.assertEqual(patient_info[4]['children'], 'Gender: Male')
+        self.assertEqual(patient_info[5]['children'], 'Age: 30')
+        self.assertEqual(patient_info[6]['children'], 'Medications Given: Medication 1, Medication 2, Medication 3')
+        self.assertEqual(patient_info[7]['children'], 'Admission Date: 2022-01-01')
+        self.assertEqual(patient_info[8]['children'], 'Expected Discharge Date: 2022-01-10')
 
-    def test_scatter_plot(self):
-        scatter_plot = code.create_scatter_plot(self.manipulated_data)
-        self.assertIsNotNone(scatter_plot)
-        self.assertIsInstance(scatter_plot, matplotlib.figure.Figure)
-
-class TestDropdownMenu(unittest.TestCase):
-    def setUp(self):
-        self.data = code.import_data()
-        self.manipulated_data = code.manipulate_data(self.data)
-
-    def test_dropdown_menu(self):
-        dropdown_menu = code.create_dropdown_menu(self.manipulated_data)
-        self.assertIsNotNone(dropdown_menu)
-        self.assertIsInstance(dropdown_menu, dash_core_components.Dropdown)
-
-if __name__ == '__main__':
+if _name_ == '_main_':
     unittest.main()
